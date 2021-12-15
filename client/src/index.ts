f0891533e134a51dd7a45de2ac84187ad4f32110
Ben Rosen <benjamin.p.rosen@gmail.com>
2021-12-15 10:39:59 -0500

    draw trees from dummy data

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,13 +1,14 @@
 import * as Phaser from "phaser";
 
 import {
+  Circle,
+  Game,
   PointerDownMessage,
   PointerUpMessage,
   Topic,
   WebSocketClient,
 } from "../../types";
 
-import { Game } from "phaser";
 import Sockette from "sockette";
 
 const authenticate = async () => {
@@ -24,17 +25,19 @@ const connectToWebSocketApi = async (
   onReconnect: (event: Event) => void,
   url: string
 ): Promise<WebSocketClient> => {
-  const webSocketClient = new Sockette(url, {
-    onclose: onClose,
-    onerror: onError,
-    onmaximum: onMaximum,
-    onmessage: onMessage,
-    onopen: onOpen,
-    onreconnect: onReconnect,
-  });
+  // const webSocketClient = new Sockette(url, {
+  //   onclose: onClose,
+  //   onerror: onError,
+  //   onmaximum: onMaximum,
+  //   onmessage: onMessage,
+  //   onopen: onOpen,
+  //   onreconnect: onReconnect,
+  // });
   return {
-    publish: (message: PointerDownMessage | PointerUpMessage) =>
-      webSocketClient.json(message),
+    publish: (message: PointerDownMessage | PointerUpMessage) => {
+      console.log(message);
+      // webSocketClient.json(message);
+    },
   };
 };
 
@@ -48,16 +51,6 @@ const handleConnectToWebSocketApiError = (error: Error) => {
   throw new Error("Not implemented.");
 };
 
-const handlePointerDownEvents = (
-  htmlElement: HTMLElement,
-  onPointerDown: (event: PointerEvent) => void
-) => htmlElement.addEventListener("pointerdown", onPointerDown);
-
-const handlePointerUpEvents = (
-  htmlElement: HTMLElement,
-  onPointerUp: (event: PointerEvent) => void
-) => htmlElement.addEventListener("pointerup", onPointerUp);
-
 const handleWebSocketError = (event: Event) => console.error(event);
 
 const handleWebSocketClose = (event: CloseEvent) =>
@@ -66,78 +59,143 @@ const handleWebSocketClose = (event: CloseEvent) =>
 const handleWebSocketMaximum = (event: Event) =>
   console.warn("WebSocket failed to reconnect; maximum retries exceeded.");
 
-// TODO call renderGameState() with event payload (game state)
-const handleWebSocketMessage = (event: MessageEvent) => console.info(event);
-
 const handleWebSocketOpen = (event: Event) =>
   console.info("WebSocket connection opened.");
 
 const handleWebSocketReconnect = (event: Event) =>
   console.info("WebSocket reconnected.");
 
-const renderGameState = (state: Game) => {
-  // TODO implement
-  throw new Error("Not implemented.");
-};
-
 const sendWebSocketMessage = <T extends PointerDownMessage | PointerUpMessage>(
   client: WebSocketClient,
   message: T
 ) => client.publish(message);
 
-const sendPointerDownWebSocketMessage = (client: WebSocketClient) =>
-  sendWebSocketMessage<PointerDownMessage>(client, {
-    position: { x: 0, y: 0 },
+const sendPointerWebSocketMessage = <
+  T extends PointerUpMessage | PointerDownMessage
+>(
+  client: WebSocketClient,
+  position: { x: number; y: number },
+  topic: Topic
+) =>
+  sendWebSocketMessage<T>(client, {
+    position: position,
     timestamp: Date.now(),
-    topic: Topic.PointerDown,
-  });
+    topic: topic,
+  } as T);
 
-const sendPointerUpWebSocketMessage = (client: WebSocketClient) =>
-  sendWebSocketMessage<PointerUpMessage>(client, {
-    position: { x: 0, y: 0 },
-    timestamp: Date.now(),
-    topic: Topic.PointerUp,
-  });
-
-// set up build script to copy html and css files to dist/ and transpile index.ts to dist/index.js
-// import this default export function and call it from index.html
-
-const createGameClient = async (
-  pointerTarget: HTMLElement,
-  webSocketApiUrl: string
-) => {
-  try {
-    await authenticate();
-  } catch (error) {
-    return handleAuthenticateError(error);
-  }
-  let webSocketClient: WebSocketClient;
-  try {
-    webSocketClient = await connectToWebSocketApi(
-      handleWebSocketClose,
-      handleWebSocketError,
-      handleWebSocketMaximum,
-      handleWebSocketMessage,
-      handleWebSocketOpen,
-      handleWebSocketReconnect,
-      webSocketApiUrl
-    );
-  } catch (error) {
-    return handleConnectToWebSocketApiError(error);
-  }
-  // TODO handle errors
-  handlePointerDownEvents(pointerTarget, () =>
-    sendPointerDownWebSocketMessage(webSocketClient)
+const sendPointerDownWebSocketMessage = (
+  client: WebSocketClient,
+  position: { x: number; y: number }
+) =>
+  sendPointerWebSocketMessage<PointerDownMessage>(
+    client,
+    position,
+    Topic.PointerDown
   );
-  // TODO handle errors
-  handlePointerUpEvents(pointerTarget, () =>
-    sendPointerUpWebSocketMessage(webSocketClient)
+
+const sendPointerUpWebSocketMessage = (
+  client: WebSocketClient,
+  position: { x: number; y: number }
+) =>
+  sendPointerWebSocketMessage<PointerDownMessage>(
+    client,
+    position,
+    Topic.PointerUp
   );
+
+const createGameClient = async (webSocketApiUrl: string) => {
+  const GAME_STATE_EVENT_NAME = "GAME_STATE_EVENT";
+  const GAME_STATE_DATA_KEY = "GAME_STATE_DATA";
+  const CLIENT_ID_DATA_KEY = "CLIENT_ID_DATA_KEY";
+
+  let graphics: Phaser.GameObjects.Graphics;
+
+  // try {
+  //   await authenticate();
+  // } catch (error) {
+  //   return handleAuthenticateError(error);
+  // }
+
+  const game = new Phaser.Game({
+    backgroundColor: 0xffffff,
+    height: "100%",
+    parent: "game",
+    scene: {
+      preload: async function (this: Phaser.Scene) {
+        // DEBUG ONLY
+        this.data.set(GAME_STATE_DATA_KEY, {
+          players: [],
+          trees: [
+            { position: { x: 0, y: 120 }, radius: 9 },
+            { position: { x: 100, y: 330 }, radius: 10 },
+            { position: { x: 35, y: 110 }, radius: 4 },
+            { position: { x: 22, y: 230 }, radius: 8 },
+            { position: { x: 5, y: 213 }, radius: 15 },
+            { position: { x: 73, y: 396 }, radius: 12 },
+            { position: { x: 300, y: 200 }, radius: 9 },
+            { position: { x: 310, y: 50 }, radius: 10 },
+            { position: { x: 235, y: 10 }, radius: 4 },
+            { position: { x: 322, y: 30 }, radius: 8 },
+            { position: { x: 225, y: 13 }, radius: 15 },
+            { position: { x: 273, y: 96 }, radius: 12 },
+          ],
+        } as Game);
+        //
+        const webSocketClient = await connectToWebSocketApi(
+          handleWebSocketClose,
+          handleWebSocketError,
+          handleWebSocketMaximum,
+          (event: MessageEvent) =>
+            game.events.emit(GAME_STATE_EVENT_NAME, event),
+          handleWebSocketOpen,
+          handleWebSocketReconnect,
+          webSocketApiUrl
+        );
+        this.game.events.on(GAME_STATE_EVENT_NAME, (event: MessageEvent) => {
+          console.log(event);
+          // TODO parse event before setting
+          this.data.set(GAME_STATE_DATA_KEY, event);
+        });
+        this.input.on(
+          Phaser.Input.Events.POINTER_DOWN,
+          (pointer: Phaser.Input.Pointer) =>
+            sendPointerDownWebSocketMessage(webSocketClient, {
+              x: pointer.x,
+              y: pointer.y,
+            })
+        );
+        this.input.on(
+          Phaser.Input.Events.POINTER_UP,
+          (pointer: Phaser.Input.Pointer) =>
+            sendPointerUpWebSocketMessage(webSocketClient, {
+              x: pointer.x,
+              y: pointer.y,
+            })
+        );
+      },
+      update: function (this: Phaser.Scene) {
+        // const clientId = this.data.get(CLIENT_ID_DATA_KEY) as string;
+        const gameState = this.data.get(GAME_STATE_DATA_KEY) as Game;
+        graphics = graphics ?? this.add.graphics();
+        graphics.clear();
+        // const clientPlayer = gameState.players.find(
+        //   (player) => player.id === clientId
+        // );
+        const drawCircle = (circle: Circle) => {
+          graphics.fillStyle(0x000000, 1);
+          graphics.fillCircle(
+            circle.position.x,
+            circle.position.y,
+            circle.radius
+          );
+        };
+        gameState.players.forEach((player) => drawCircle(player));
+        gameState.trees.forEach((tree) => drawCircle(tree));
+        // TODO camera follow clientPlayer position
+      },
+    },
+    width: "100%",
+  });
 };
 
-new Phaser.Game({
-  backgroundColor: 0x0000ff,
-  parent: "game",
-  height: "100%",
-  width: "100%",
-});
+createGameClient("");
