40c9fd9dafd86a495ca9117eab00d97c58992a63
Ben Rosen <benjamin.p.rosen@gmail.com>
2021-12-25 00:13:42 -0500

    add logdriver; configure health check status code

--- service/src/constructs/websocket-server.ts
+++ service/src/constructs/websocket-server.ts
@@ -2,7 +2,7 @@
 
 // Source example: https://github.com/aws-samples/aws-cdk-examples/blob/master/typescript/ecs/fargate-service-with-local-image/index.ts
 
-import { Cluster, ContainerImage } from "@aws-cdk/aws-ecs";
+import { AwsLogDriver, Cluster, ContainerImage } from "@aws-cdk/aws-ecs";
 import { Construct, Stack } from "@aws-cdk/core";
 
 import { ApplicationLoadBalancedFargateService } from "@aws-cdk/aws-ecs-patterns";
@@ -18,6 +18,7 @@ export class WebSocketServer extends Construct {
     super(parent, name);
     const vpc = new Vpc(parent, "WebSocketServerVpc", { maxAzs: 2 });
     const cluster = new Cluster(parent, "WebSocketServerCluster", { vpc });
+    const logDriver = new AwsLogDriver({ streamPrefix: "WebSocketServer" });
     new ApplicationLoadBalancedFargateService(
       parent,
       "WebSocketServerFargateService",
@@ -26,8 +27,9 @@ export class WebSocketServer extends Construct {
         taskImageOptions: {
           containerPort: props.containerPort,
           image: ContainerImage.fromAsset(props.containerPath),
+          logDriver,
         },
       }
-    );
+    ).targetGroup.configureHealthCheck({ healthyHttpCodes: "426" });
   }
 }
