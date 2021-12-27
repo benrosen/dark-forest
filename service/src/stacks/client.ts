b367b66edc5d1a0225f7154796224751ad4c78d0
Ben Rosen <benjamin.p.rosen@gmail.com>
2021-12-26 20:42:33 -0500

    use stack names from package.json

--- service/src/stacks/client.ts
+++ service/src/stacks/client.ts
@@ -1,10 +1,13 @@
 import { Construct, Stack, StackProps } from "@aws-cdk/core";
 
 import { StaticSite } from "../constructs/static-site";
+import { data } from "../../package.json";
+
+const { stacks } = data.input;
 
 export default class Client extends Stack {
   constructor(scope: Construct, props?: StackProps) {
-    super(scope, "TDF_ClientStack", props);
+    super(scope, stacks.client, props);
     new StaticSite(this, "StaticSite", {
       domainName: this.node.tryGetContext("domain"),
       siteSubDomain: this.node.tryGetContext("subdomain"),
--- service/src/stacks/service.ts
+++ service/src/stacks/service.ts
@@ -1,10 +1,13 @@
 import { Construct, Stack, StackProps } from "@aws-cdk/core";
 
 import { WebSocketServer } from "../constructs/websocket-server";
+import { data } from "../../package.json";
+
+const { stacks } = data.input;
 
 export default class Service extends Stack {
   constructor(scope: Construct, props?: StackProps) {
-    super(scope, "TDF_ServiceStack", props);
+    super(scope, stacks.service, props);
     new WebSocketServer(this, "WebSocketServer", {
       containerPath: this.node.tryGetContext("containerPath"),
       containerPort: this.node.tryGetContext("containerPort"),
