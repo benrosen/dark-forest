ea0aa75fefb83ca474d30e60aead1f47fc571bf7
Ben Rosen <benjamin.p.rosen@gmail.com>
2021-12-31 21:58:23 -0500

    upgrade ws to wss

--- service/src/constructs/websocket-server.ts
+++ service/src/constructs/websocket-server.ts
@@ -1,16 +1,23 @@
 #!/usr/bin/env node
 
-// Source example: https://github.com/aws-samples/aws-cdk-examples/blob/master/typescript/ecs/fargate-service-with-local-image/index.ts
+// Source example (Fargate): https://github.com/aws-samples/aws-cdk-examples/blob/master/typescript/ecs/fargate-service-with-local-image/index.ts
+// Source example (ALB HTTPS): https://docs.aws.amazon.com/cdk/api/v1/docs/aws-ecs-patterns-readme.html#configure-https-on-an-applicationloadbalancedfargateservice
+
+import * as route53 from "@aws-cdk/aws-route53";
 
 import { AwsLogDriver, Cluster, ContainerImage } from "@aws-cdk/aws-ecs";
 import { Construct, Stack } from "@aws-cdk/core";
 
 import { ApplicationLoadBalancedFargateService } from "@aws-cdk/aws-ecs-patterns";
+import { Certificate } from "@aws-cdk/aws-certificatemanager";
+import { SslPolicy } from "@aws-cdk/aws-elasticloadbalancingv2";
 import { Vpc } from "@aws-cdk/aws-ec2";
 
 export interface WebSocketServerProps {
+  apiSubDomain: string;
   containerPath: string;
   containerPort: number;
+  domainName: string;
 }
 
 export class WebSocketServer extends Construct {
@@ -19,11 +26,26 @@ export class WebSocketServer extends Construct {
     const vpc = new Vpc(parent, "WebSocketServerVpc", { maxAzs: 2 });
     const cluster = new Cluster(parent, "WebSocketServerCluster", { vpc });
     const logDriver = new AwsLogDriver({ streamPrefix: "WebSocketServer" });
+    const domainZone = route53.HostedZone.fromLookup(this, "Zone", {
+      domainName: props.domainName,
+    });
+    const apiDomain = props.apiSubDomain + "." + props.domainName;
+    const certificate = Certificate.fromCertificateArn(
+      this,
+      "Cert",
+      // TODO get cert programmatically
+      "arn:aws:acm:us-east-1:326238338403:certificate/79b300ec-8cb9-438f-89ff-3a1e8ceb0d12"
+    );
     new ApplicationLoadBalancedFargateService(
       parent,
       "WebSocketServerFargateService",
       {
         cluster,
+        certificate,
+        sslPolicy: SslPolicy.RECOMMENDED,
+        domainName: apiDomain,
+        domainZone,
+        redirectHTTP: true,
         taskImageOptions: {
           containerPort: props.containerPort,
           image: ContainerImage.fromAsset(props.containerPath),
--- service/src/stacks/service.ts
+++ service/src/stacks/service.ts
@@ -9,8 +9,10 @@ export default class Service extends Stack {
   constructor(scope: Construct, props?: StackProps) {
     super(scope, stacks.service, props);
     new WebSocketServer(this, "WebSocketServer", {
+      apiSubDomain: this.node.tryGetContext("apiSubDomain"),
       containerPath: this.node.tryGetContext("containerPath"),
       containerPort: this.node.tryGetContext("containerPort"),
+      domainName: this.node.tryGetContext("domain"),
     });
   }
 }
